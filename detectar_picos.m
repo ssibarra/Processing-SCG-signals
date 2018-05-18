085a430acd5ee06d1124397502dccdc750c05951
Saul Ibarra <Saul@MacBook-Pro-de-Saul.local>
2018-05-17 21:10:38 -0500

    First signal

--- detectar_picos.m
+++ detectar_picos.m
@@ -1,11 +1,12 @@
 function detectar_picos
-signal=load('datos.csv', '-ascii');
-x=signal(:,1);
-y=signal(:,2);
-plot(x,y, 'r-')
-hold on
-[peak_value, peak_location] = findpeaks(signal);
-plot (peak_location, peak_value, 'c')
-[peak_value, peak_location] = findpeaks(signal,'sortstr','ascend');
-
+signal=load('b001m.mat');
+%x=signal(:,1);
+%y=signal(:,2);
+%plot(x,y, 'r-')
+figure ();
+plot (signal);
+%hold on
+%[peak_value, peak_location] = findpeaks(signal);
+%plot (peak_location, peak_value, 'c')
+%[peak_value, peak_location] = findpeaks(signal,'sortstr','ascend');
 end
